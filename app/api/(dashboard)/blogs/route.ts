7076622e4e1829d777e1e27ea64eb272e8fe07bd
trevonakesh <trevonbusiness1@gmail.com>
2025-03-26 14:42:53 +0530

    ADD: created the POST & GET in the blog

--- app/api/(dashboard)/blogs/route.ts
+++ app/api/(dashboard)/blogs/route.ts
@@ -6,19 +6,19 @@ import Category from "@/lib/models/category";
 import Blog from "@/lib/models/blog";
 
 export const GET = async (request: Request) => {
-    try{
-        const { searchParams} = new URL(request.url);
+    try {
+        const { searchParams } = new URL(request.url);
         const userId = searchParams.get("userId");
         const categoryId = searchParams.get("categoryId");
 
-        if(!userId || !Types.ObjectId.isValid(userId)){
+        if (!userId || !Types.ObjectId.isValid(userId)) {
             return new NextResponse(
                 JSON.stringify({ message: "Invalid or missing userId" }),
                 { status: 400 }
             );
         }
 
-        if(!categoryId || !Types.ObjectId.isValid(categoryId)){
+        if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
             return new NextResponse(
                 JSON.stringify({ message: "Invalid or missing categoryID" }),
                 { status: 400 }
@@ -40,19 +40,77 @@ export const GET = async (request: Request) => {
                 { status: 400 });
         }
 
-    const filter: any = {
-        user: new Types.ObjectId(userId),
-        category: new Types.ObjectId(categoryId),
-    };
+        const filter: any = {
+            user: new Types.ObjectId(userId),
+            category: new Types.ObjectId(categoryId),
+        };
 
 
-    // TODO 
+        // TODO 
 
-    const blogs = await Blog.find(filter);
+        const blogs = await Blog.find(filter);
 
-    return new NextResponse(JSON.stringify({ blogs }), { status: 200 });
+        return new NextResponse(JSON.stringify({ blogs }), { status: 200 });
 
-    }catch(error: any){
+    } catch (error: any) {
         return new NextResponse("Error in getting blogs" + error.message, { status: 500 });
     }
 }
+
+
+export const POST = async (request: Request) => {
+    try {
+        const { searchParams } = new URL(request.url);
+        const userId = searchParams.get("userId");
+        const categoryId = searchParams.get("categoryId");
+
+        const body = await request.json();
+        const { title, description } = body;
+
+
+        if (!userId || !Types.ObjectId.isValid(userId)) {
+            return new NextResponse(
+                JSON.stringify({ message: "Invalid or missing userId" }),
+                { status: 400 }
+            );
+        }
+
+        if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
+            return new NextResponse(
+                JSON.stringify({ message: "Invalid or missing categoryID" }),
+                { status: 400 }
+            );
+        }
+
+        await connect();
+
+        const user = await User.findById(userId);
+
+        if (!user) {
+            JSON.stringify({ massage: "User not found in the database" }), { status: 400 }
+        }
+
+        const category = await Category.findById(categoryId);
+
+        if (!category) {
+            return new NextResponse(JSON.stringify({ message: "category not found" }),
+                { status: 400 });
+        }
+
+        const newBlog = new Blog({
+            title,
+            description,
+            user: new Types.ObjectId(userId),
+            category: new Types.ObjectId(categoryId),
+        });
+
+        await newBlog.save();
+
+        return new NextResponse(JSON.stringify({ message: "Blog created", blog: newBlog }),
+            { status: 200 });
+
+    } catch (error: any) {
+        return new NextResponse("Error in creating blog" + error.message,
+            { status: 500 });
+    }
+} 
